0cfe3ba5967536de8315d34b9d611174ac8710ce
hogwarts_Ad <lixu@ceshiren.com>
2022-04-25 11:15:40 +0800

    比赛标准代码python版本

--- web/advanced/page/shopping_cart.py
+++ web/advanced/page/shopping_cart.py
@@ -5,10 +5,12 @@ __desc__ = '更多测试开发技术探讨，请访问：https://ceshiren.com/t/
 from selenium.webdriver.common.by import By
 
 from web.advanced.page.base_page import BasePage
+from web.advanced.utils.log_utils import logger
 
 
 class ShoppingCart(BasePage):
     def get_product_list(self):
         res = self.finds(By.CSS_SELECTOR, ".van-card__title")
         res_text = [i.text for i in res]
+        logger.info(f"购物车的商品有{res_text}")
         return res_text
